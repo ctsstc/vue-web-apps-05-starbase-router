c92978f1d00add09da39c04daf1a00ed73d9086d
Cody Swartz <ctsstc@gmail.com>
2018-03-29 22:47:48 -0700

    5.38 Developing the Data Component
    - Routing links
    - When you navigate the data fetches and comes back to display

--- src/components/Data.vue
+++ src/components/Data.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="">
     Type: {{type}}
+    <br><br>
+    {{items}}
   </div>
 </template>
 
@@ -8,8 +10,33 @@
   export default {
     data() {
       return {
-        type: this.$route.params.type
+        type: this.$route.params.type,
+        items: []
       }
+    },
+    watch: {
+      '$route': 'fetchItems'
+    },
+    methods: {
+      fetchItems() {
+        this.items = [];
+        this.type = this.$route.params.type;
+        let initial_ids = [1, 13, 14];
+
+        for (let i in initial_ids) {
+          let id = initial_ids[i];
+          console.log({id});
+          fetch(
+            `https://swapi.co/api/${this.type}/${id}/`, 
+            { method: 'GET' }
+          )
+          .then(res => res.json())
+          .then(json => this.items.push(json));
+        }
+      }
+    },
+    created() {
+      this.fetchItems()
     }
   }
 </script>
